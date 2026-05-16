688ff921b81da7361b0bedd029c7b29a3ab6ab2f
pch6630 <pch6640@gmail.com>
2026-05-16 15:57:24 +0900

    Update zygisk.hpp

--- native/zygisk.hpp
+++ native/zygisk.hpp
@@ -1,23 +1,95 @@
-/* Copyright 2022-2023 John "topjohnwu" Wu ... (생략) */
+/* Copyright 2022-2023 John "topjohnwu" Wu
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #pragma once
+
 #include <jni.h>
 #include <stdint.h>
 
 #define ZYGISK_API_VERSION 3
 
 namespace zygisk {
-// ... (클래스 정의: Api, AppSpecializeArgs, ServerSpecializeArgs 등 상동)
-class ModuleBase { /* ... */ };
-struct AppSpecializeArgs { /* ... */ };
-struct ServerSpecializeArgs { /* ... */ };
-enum Option : int { /* ... */ };
-enum StateFlag : uint32_t { /* ... */ };
-struct Api { /* ... */ };
+
+struct Api;
+struct AppSpecializeArgs;
+struct ServerSpecializeArgs;
+
+class ModuleBase {
+public:
+    virtual void onLoad([[maybe_unused]] Api *api, [[maybe_unused]] JNIEnv *env) {}
+    virtual void preAppSpecialize([[maybe_unused]] AppSpecializeArgs *args) {}
+    virtual void postAppSpecialize([[maybe_unused]] const AppSpecializeArgs *args) {}
+    virtual void preServerSpecialize([[maybe_unused]] ServerSpecializeArgs *args) {}
+    virtual void postServerSpecialize([[maybe_unused]] const ServerSpecializeArgs *args) {}
+};
+
+struct AppSpecializeArgs {
+    jint &uid;
+    jint &gid;
+    jintArray &gids;
+    jint &runtime_flags;
+    jobjectArray &rlimits;
+    jint &mount_external;
+    jstring &se_info;
+    jstring &nice_name;
+    jstring &instruction_set;
+    jstring &app_data_dir;
+
+    jintArray *const fds_to_ignore;
+    jboolean *const is_child_zygote;
+    jboolean *const is_top_app;
+    jobjectArray *const pkg_data_info_list;
+    jobjectArray *const whitelisted_data_info_list;
+    jboolean *const mount_data_dirs;
+    jboolean *const mount_storage_dirs;
+
+    AppSpecializeArgs() = delete;
+};
+
+struct ServerSpecializeArgs {
+    jint &uid;
+    jint &gid;
+    jintArray &gids;
+    jint &runtime_flags;
+    jlong &permitted_capabilities;
+    jlong &effective_capabilities;
+
+    ServerSpecializeArgs() = delete;
+};
+
+enum Option : int {
+    FORCE_DENYLIST_UNMOUNT = 0,
+    DLCLOSE_MODULE_LIBRARY = 1,
+};
+
+enum StateFlag : uint32_t {
+    PROCESS_GRANTED_ROOT = (1u << 0),
+    PROCESS_ON_DENYLIST = (1u << 1),
+};
+
+struct Api {
+    int connectCompanion();
+    uint32_t getFlags();
+    void setOption(Option opt);
+    void hookJniNativeMethods(JNIEnv *env, const char *className, const JNINativeMethod *methods, int numMethods);
+    void *reserved;
+};
 
 namespace internal {
-// 템플릿 빌드용 내부 구조 정의 및 ABI 매핑 기능
+
 struct api_table {
-    void *v1[4], *v2[1], *v3[1];
+    void *v1, *v2, *v3;
     void (*registerModule)(api_table *, void *);
 };
 
@@ -33,13 +105,30 @@ struct module_abi {
 
 template <class T>
 void entry_impl(api_table *table, JNIEnv *env) {
-    // ABI 바인딩 로직
-    // ... (상동)
+    static module_abi abi{
+        .api_version = ZYGISK_API_VERSION,
+        .impl = nullptr,
+        .onLoad = [](Api *api, JNIEnv *env) {
+            abi.impl = new T();
+            abi.impl->onLoad(api, env);
+        },
+        .preAppSpecialize = [](AppSpecializeArgs *args) { abi.impl->preAppSpecialize(args); },
+        .postAppSpecialize = [](const AppSpecializeArgs *args) { abi.impl->postAppSpecialize(args); },
+        .preServerSpecialize = [](ServerSpecializeArgs *args) { abi.impl->preServerSpecialize(args); },
+        .postServerSpecialize = [](const ServerSpecializeArgs *args) { abi.impl->postServerSpecialize(args); }
+    };
     table->registerModule(table, &abi);
 }
+
 } // namespace internal
 } // namespace zygisk
 
-// Zygisk 모듈 등록 매크로 (필수)
-#define REGISTER_ZYGISK_MODULE(clazz) ...
-#define REGISTER_ZYGISK_COMPANION(func) ...
+#define REGISTER_ZYGISK_MODULE(clazz) \
+    extern "C" [[gnu::visibility("default")]] void zygisk_module_entry(::zygisk::internal::api_table *table, JNIEnv *env) { \
+        ::zygisk::internal::entry_impl<clazz>(table, env); \
+    }
+
+#define REGISTER_ZYGISK_COMPANION(func) \
+    extern "C" [[gnu::visibility("default")]] void zygisk_companion_entry(int client_socket) { \
+        func(client_socket); \
+    }
