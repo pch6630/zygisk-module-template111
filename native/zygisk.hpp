18794a7a26f3786a18c3bbad36e40dbbe22a6bb6
pch6630 <pch6640@gmail.com>
2026-05-16 15:50:40 +0900

    Update zygisk.hpp

--- native/zygisk.hpp
+++ native/zygisk.hpp
@@ -1,17 +1,3 @@
-/* Copyright 2022-2023 John "topjohnwu" Wu
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
- * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- */
-
 #pragma once
 
 #include <jni.h>
@@ -84,7 +70,6 @@ enum StateFlag : uint32_t {
 };
 
 struct Api {
-    // API v3 (Magisk 25200) 규격 전용 구조체 정의
     int connectCompanion();
     uint32_t getFlags();
     void setOption(Option opt);
@@ -94,7 +79,6 @@ struct Api {
 
 } // namespace zygisk
 
-// 모듈 등록 매크로
 #define REGISTER_ZYGISK_MODULE(clazz) \
     extern "C" [[gnu::visibility("default")]] void zygisk_module_entry(::zygisk::internal::api_table *table, JNIEnv *env) { \
         ::zygisk::internal::entry_impl<clazz>(table, env); \
